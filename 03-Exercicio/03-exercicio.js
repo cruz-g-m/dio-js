43ef197c487c55659e5af46de15c0a7790c66f2d
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-11-23 19:44:43 -0300

    Conclusão do desafio

--- 03-Exercicio/03-exercicio.js
+++ 03-Exercicio/03-exercicio.js
@@ -1,8 +1,8 @@
-let str = "Esse desafio foi facil"
+let str = "Education best team"
 let branco = []
-let vogais = []
 let countBranco = 0
 let countVogais = 0
+
 
 let strSplit = str.split('');
 
@@ -14,18 +14,57 @@ for (let i = 0; i < strSplit.length; i++) {
 }
 }
 
+let strMatch = str.match(/[aeiou]/gi);
 
-for (let x = 0; x < strSplit.length; x++) {
-    let strVogais = strSplit[x]
-        const regexp = /[A-E-I-O-U-a]/gi
-        if(strVogais.match(regexp)){
-            vogais.push()
-            countVogais++
+for (let x = 0; x < strMatch.length; x++) {
+    let strVogais = strMatch[x]
+    countVogais++
+        
+            
 }    
-}
 
-resultadoFinalVogais = countVogais - countBranco
 
 console.log('Tem ' + countBranco + ' Espaços em branco')
-console.log('Tem ' + resultadoFinalVogais + ' Vogais')
+console.log('Tem ' + countVogais + ' Vogais')
+
+
+/**
+ * 
+ * // IMPORTANTE: As funções "gets" e "print" são acessíveis globalmente, onde:
+// - "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+// - ".split()": esse método transforma uma string em um array de substrings;
+// - "print": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente.
+
+// TODO: faça um programa que contabilize quantos espaços em branco e quantas vogais existem
+//na string de entrada;
+
+let str = gets();
+
+let branco = []
+let countBranco = 0
+let countVogais = 0
+
+
+let strSplit = str.split('');
+
+for (let i = 0; i < strSplit.length; i++) {
+    let strBranco = strSplit[i]
+        if(strBranco.match(' ')){
+            branco.push()
+            countBranco++
+}
+}
+
+let strMatch = str.match(/[aeiou]/gi);
+
+for (let x = 0; x < strMatch.length; x++) {
+    let strVogais = strMatch[x]
+    countVogais++
+        
+            
+}    
+
+
+print('Espacos em branco: ' + countBranco + ' Vogais: ' + countVogais)
+ */
 
